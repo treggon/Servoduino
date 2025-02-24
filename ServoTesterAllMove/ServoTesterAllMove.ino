f53634898f039bb75ad6f0e614f7446a12834af4
treggonowens <treggon@tregtronics.com>
2025-02-23 20:22:49 -0800

    Started this.

---
+++ ServoTesterAllMove/ServoTesterAllMove.ino
@@ -0,0 +1,116 @@
+/*************************************************** 
+Servo Tester, code modified by Treggon Owens from
+Default adafruit code.
+
+Making them all move at once
+ ****************************************************/
+//#include <ros.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+// Setup the ros node handler
+
+//ros::NodeHandle nh;
+
+char hello[13] = "Hello world!";
+
+// Depending on your servo make, the pulse width min and max may vary, you 
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN  450 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2450 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 60 // Analog servos run at ~50 Hz updates
+#define SERVO_MIDPOINT 1500
+
+#define DELTA_FREQ 1
+
+// our servo # counter
+uint8_t servonum = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8 channel Servo test!");
+
+  pwm.begin();
+  /*
+   * In theory the internal oscillator (clock) is 25MHz but it really isn't
+   * that precise. You can 'calibrate' this by tweaking this number until
+   * you get the PWM update frequency you're expecting!
+   * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
+   * is used for calculating things like writeMicroseconds()
+   * Analog servos run at ~50 Hz updates, It is importaint to use an
+   * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
+   * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
+   *    the I2C PCA9685 chip you are setting the value for.
+   * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
+   *    expected value (50Hz for most ESCs)
+   * Setting the value here is specific to each individual I2C PCA9685 chip and
+   * affects the calculations for the PWM update frequency. 
+   * Failure to correctly set the int.osc value will cause unexpected PWM results
+   */
+  pwm.setOscillatorFrequency(27000000);
+
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+
+  for(int servonumber = 0; servonumber<16; servonumber++)
+  {
+    pwm.writeMicroseconds(servonumber, SERVO_MIDPOINT);
+  }
+
+  //nh.initNode();
+  delay(10);
+}
+
+// You can use this function if you'd like to set the pulse length in seconds
+// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
+void setServoPulse(uint8_t n, double pulse) {
+  double pulselength;
+  
+  pulselength = 1000000;   // 1,000,000 us per second
+  pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
+  Serial.print(pulselength); Serial.println(" us per period"); 
+  pulselength /= 4096;  // 12 bits of resolution
+  Serial.print(pulselength); Serial.println(" us per bit"); 
+  pulse *= 1000000;  // convert input seconds to us
+  pulse /= pulselength;
+  Serial.println(pulse);
+  pwm.setPWM(n, 0, pulse);
+}
+
+void loop() {
+  
+  // Drive each servo one at a time using setPWM()
+  Serial.println(servonum +"=Servo#");
+  
+  uint16_t microsec = SERVO_MIDPOINT;
+  // Drive each servo one at a time using writeMicroseconds(), it's not precise due to calculation rounding!
+  // The writeMicroseconds() function is used to mimic the Arduino Servo library writeMicroseconds() behavior. 
+  for (microsec = SERVO_MIDPOINT; microsec < USMAX; microsec+=DELTA_FREQ) {
+    pwm.writeMicroseconds(servonum, microsec);
+    //Serial.println(microsec);
+  }
+  Serial.println(microsec);
+
+  delay(500);
+  for (microsec = USMAX; microsec > USMIN; microsec-=DELTA_FREQ) {
+    pwm.writeMicroseconds(servonum, microsec);
+    //Serial.println(microsec);
+  }
+  Serial.println(microsec);
+  delay(500);
+  for (microsec = USMIN; microsec < SERVO_MIDPOINT; microsec+=DELTA_FREQ) {
+    pwm.writeMicroseconds(servonum, microsec);
+    //Serial.println(microsec);
+  }
+  Serial.println(microsec);
+  delay(500);
+
+  servonum++;
+  if (servonum > 7) servonum = 0; // Testing the first 8 servo channels
+}
